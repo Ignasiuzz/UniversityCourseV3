64bba69dc08a2b67bb15e95dfe17a17d53eddbc4
Ignasiuzz <ignolaiskai@gmail.com>
2024-05-20 19:06:19 +0300

    Iterators and Capacity testing
    Prideti testai iterators and capacity

--- Unit_Tests/Unit_Test.cpp
+++ Unit_Tests/Unit_Test.cpp
@@ -75,3 +75,75 @@ TEST(Element_Access, Back) {
 
     EXPECT_EQ(back.back(), 30);
 }
+
+/* Iterators */
+TEST(Iterators, Begin_End_Iterators) {
+    vectorClass<int> begin;
+    begin.push_back(1);
+    begin.push_back(2);
+    begin.push_back(3);
+
+    int expected_value = 1;
+    for (auto it = begin.begin(); it != begin.end(); ++it) {
+        EXPECT_EQ(expected_value, *it);
+        ++expected_value;
+    }
+}
+
+TEST(Iterators, RBegin_REnd_Iterators) {
+    vectorClass<int> RBegin;
+    RBegin.push_back(1);
+    RBegin.push_back(2);
+    RBegin.push_back(3);
+
+    int expected_value = 3;
+    for (auto it = RBegin.rbegin(); it != RBegin.rend(); ++it) {
+        EXPECT_EQ(expected_value, *it);
+        --expected_value;
+    }
+}
+
+/* Capacity */
+TEST(Capacity, Empty) {
+    vectorClass<int> empty;
+    EXPECT_TRUE(empty.empty());
+
+    empty.push_back(10);
+    EXPECT_FALSE(empty.empty());
+}
+
+TEST(Capacity, Size) {
+    vectorClass<int> size;
+    EXPECT_EQ(size.size(), 0);
+
+    size.push_back(1);
+    size.push_back(2);
+    size.push_back(3);
+
+    EXPECT_EQ(size.size(), 3);
+}
+
+TEST(Capacity, MaxSize) {
+    vectorClass<int> MaxSize;
+    EXPECT_GE(MaxSize.max_size(), 0); // EXPECT_GE - expect greater
+}
+
+TEST(Capacity, Reserve_Capacity) {
+    vectorClass<int> reserve;
+    reserve.reserve(10);
+    EXPECT_GE(reserve.get_capacity(), 10);
+
+    reserve.reserve(5);
+    EXPECT_GE(reserve.get_capacity(), 10);
+}
+
+TEST(Capacity, Shrink_To_Fit) {
+    vectorClass<int> shrink;
+    shrink.reserve(100);
+    shrink.push_back(10);
+    shrink.push_back(20);
+    shrink.push_back(30);
+    shrink.shrink_to_fit();
+    EXPECT_EQ(shrink.size(), 3);
+    EXPECT_EQ(shrink.get_capacity(), 3);
+}
