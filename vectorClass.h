5af34949eefac100977361cb83d45ae150bafad6
Ignasiuzz <ignolaiskai@gmail.com>
2024-05-20 18:59:03 +0300

    Iterators
    Added Iterators

--- vectorClass.h
+++ vectorClass.h
@@ -34,5 +34,19 @@ public:
     T& back();
     const T& back() const;
     /* Truksta data */
+
+    // Iterators
+    typedef T* iterator;
+    typedef const T* const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+    iterator begin() noexcept { return arr; }
+    const_iterator begin() const noexcept { return arr; }
+    iterator end() noexcept { return arr + current; }
+    const_iterator end() const noexcept { return arr + current; }
+    reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+    reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
 };
 #endif // VECTORCLASS_H
