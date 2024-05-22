30929f7522b964a4d3fb572b4cce4c48b15e5567
Ignasiuzz <ignolaiskai@gmail.com>
2024-05-22 12:30:02 +0300

    Cleanup
    Sutvarkyta repozitorija

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV3
-# Build directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("Unit_Tests")
